1f8648b5739567de5aae9c62c901214d0864c7b2
Frank Williames <dev.frankw@gmail.com>
2023-07-18 02:32:24 -0400

    add Item model II

--- src/main/java/com/gamefactory/bag/model/Item.java
+++ src/main/java/com/gamefactory/bag/model/Item.java
@@ -1,4 +1,48 @@
 package com.gamefactory.bag.model;
 
-public class Item {
+public class Item{
+
+    private String name;
+    private String description;
+    /*add other attributes*/
+
+    Item(String newName, String newDescription){
+        this.setName(newName);
+        this.setDescription(newDescription);
+    }
+
+    private void setName(String newName){
+        this.name = newName;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    private void setDescription(String newDescription){
+        this.description = newDescription;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void itemInformation(){
+
+        /*Formatting the Name to Uppercase */
+        String tempName = this.getName();
+        tempName = tempName.substring(0, 1).toUpperCase() + tempName.substring(1);
+
+        /*Formatting the Description to Uppercase */
+        String tempDescription = this.getDescription();
+        tempDescription = tempDescription.substring(0, 1).toUpperCase() + tempDescription.substring(1);
+
+        /*Create the itemInformation*/
+        String temp = "Nome: " + tempName +"\n" + "Descricao: " + tempDescription + "\n";
+
+        /*Print the Information*/
+        System.out.println(temp);
+    }
+
 }
+
